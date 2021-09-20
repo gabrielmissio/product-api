4d45e1f0c403d7e2426150fd52a25ef9994a52df
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-20 10:48:37 -0300

    refactor(login-router): remove duplicated code

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,7 @@
 class LoginRouter {
   async route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    const { email, password } = httpRequest;
+    if (!email || !password) {
       return {
         statusCode: 400
       }
