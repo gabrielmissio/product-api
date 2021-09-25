ad7279a2f95ea774f7d76b9f396d25bc3655b925
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-25 20:17:10 -0300

    chore: remove unused invalid-param-error helper

--- src/domain/helpers/errors/index.js
+++ src/domain/helpers/errors/index.js
@@ -1,7 +1,5 @@
 const MissingParamError = require('./missing-param-error');
-const InvalidParamError = require('./invalid-param-error');
 
 module.exports = {
-  MissingParamError,
-  InvalidParamError
+  MissingParamError
 };
