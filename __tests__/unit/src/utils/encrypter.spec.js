b8d560c75c708adebb458682ad59a34d97870ce1
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-26 21:02:42 -0300

    feat(encrypter): ensure Encrypter returns false if bcrypt returns false

--- __tests__/unit/src/utils/encrypter.spec.js
+++ __tests__/unit/src/utils/encrypter.spec.js
@@ -20,7 +20,17 @@ describe('Given the encrypter', () => {
       const { sut } = makeSut();
       const isValid = await sut.compare('any_password', 'any_hashed_password');
 
-      expect(isValid).toBeTruthy();
+      expect(isValid).toBe(true);
+    });
+  });
+
+  describe('And bcrypt returns false', () => {
+    test('Then I expect it returns false', async() => {
+      const { sut } = makeSut();
+      bcrypt.isValid = false;
+      const isValid = await sut.compare('any_password', 'any_hashed_password');
+
+      expect(isValid).toBe(false);
     });
   });
 });
