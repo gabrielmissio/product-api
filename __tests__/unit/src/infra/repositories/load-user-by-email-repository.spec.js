844d3408d58a5dd0d935b3a15689efda6542ea64
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-28 22:31:02 -0300

    feat(load-user-by-email-repository): ensure LoadUserByEmail Repository returns a user if an existing email is provided

--- __tests__/unit/src/infra/repositories/load-user-by-email-repository.spec.js
+++ __tests__/unit/src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,23 +1,51 @@
-const makeSut = () => {
-  class LoadUserByEmailRepository {
-    async load(email) {
-      return null;
-    };
-  }
-  const sut = new LoadUserByEmailRepository();
+const { MongoClient } = require('mongodb');
 
-  return{
-    sut
+class LoadUserByEmailRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
+
+  async load(email) {
+    const user = this.userModel.findOne({ email });
+    return user;
   };
-};
+}
 
 describe('Given the LoadUserByEmail Repository', () => {
-  describe('And no user is found', () => {
+  let client, db;
+
+  beforeAll(async () => {
+    client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = client.db();
+  });
+
+  afterAll(async () => {
+    await client.close();
+  });
+
+  describe('And provide a non-existent email', () => {
     test('Then I expect it returns null', async() => {
-      const { sut } = makeSut();
-      const user = await sut.load('any@mail.com');
+      const userModel = db.collection('users');
+      const sut = new LoadUserByEmailRepository(userModel);
+      const user = await sut.load('non_existent@email.com');
 
       expect(user).toBeNull();
+    });
+  });
+
+  describe('And provide an existing email', () => {
+    test('Then I expect it returns a user', async() => {
+      const userModel = db.collection('users');
+      await userModel.insertOne({
+        email: 'valid@mail.com'
+      });
+      const sut = new LoadUserByEmailRepository(userModel);
+      const user = await sut.load('valid@mail.com');
+
+      expect(user.email).toBe('valid@mail.com');
     });
   });
 });
