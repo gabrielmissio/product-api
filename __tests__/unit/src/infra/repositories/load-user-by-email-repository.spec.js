cd18241b458b36eb0930b2fce85508ecd5a477e8
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-29 21:15:27 -0300

    refactor(load-user-by-email-repository): move sut creation to a factory method

--- __tests__/unit/src/infra/repositories/load-user-by-email-repository.spec.js
+++ __tests__/unit/src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,9 +1,18 @@
 const { MongoClient } = require('mongodb');
 const LoadUserByEmailRepository = require('./../../../../../src/infra/repositories/load-user-by-email-repository');
+let client, db;
+
+const makeSut = () => {
+  const userModel = db.collection('users');
+  const sut = new LoadUserByEmailRepository(userModel);
+
+  return {
+    sut,
+    userModel
+  };
+};
 
 describe('Given the LoadUserByEmail Repository', () => {
-  let client, db;
-
   beforeAll(async () => {
     client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -22,8 +31,7 @@ describe('Given the LoadUserByEmail Repository', () => {
 
   describe('And provide a non-existent email', () => {
     test('Then I expect it returns null', async() => {
-      const userModel = db.collection('users');
-      const sut = new LoadUserByEmailRepository(userModel);
+      const { sut } = makeSut();
       const user = await sut.load('non_existent@email.com');
 
       expect(user).toBeNull();
@@ -32,11 +40,10 @@ describe('Given the LoadUserByEmail Repository', () => {
 
   describe('And provide an existing email', () => {
     test('Then I expect it returns a user', async() => {
-      const userModel = db.collection('users');
+      const { sut, userModel } = makeSut();
       await userModel.insertOne({
         email: 'valid@mail.com'
       });
-      const sut = new LoadUserByEmailRepository(userModel);
       const user = await sut.load('valid@mail.com');
 
       expect(user.email).toBe('valid@mail.com');
