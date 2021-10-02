dcd58e00f17ca6a91cfe3b2813f0027337278b54
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-02 13:47:36 -0300

    feat(env): add token secret parameter

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/product-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/product-api',
+  secret: process.env.TOKEN_SECRET || 'secret'
 };
