7aba0ec08ddbf3f7f05d35fc7f7cad1559cc98d2
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-01 22:30:55 -0300

    chore(cors-middleware): adjust main description

--- __tests__/unit/src/main/middlewares/cors.spec.js
+++ __tests__/unit/src/main/middlewares/cors.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('./../../../../../src/main/config/app');
 
-describe('Given the App Setup', () => {
+describe('Given the CORS Middleware', () => {
   describe('And the cors settings is checked', () => {
     test('Then I expect its value is *', async() => {
       app.get('/test_cors', (req, res) => {
