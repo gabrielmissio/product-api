37164373f81fa1d432f6043f356dd640ea14ed5f
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-01 22:25:42 -0300

    chore(app-setup): move cors to its own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,8 +1,5 @@
+const cors = require('./../middlewares/cors');
+
 module.exports = app => {
-  app.use((req, res, next) => {
-    res.set('access-control-allow-origin', '*');
-    res.set('access-control-allow-methods', '*');
-    res.set('access-control-allow-headers', '*');
-    next();
-  });
+  app.use(cors);
 };
