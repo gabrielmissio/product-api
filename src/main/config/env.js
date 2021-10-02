8b3d4f7d26b201bc0326e1e8322db86e8368f149
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-01 23:19:27 -0300

    chore(app): connect to the database before running the server

---
+++ src/main/config/env.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/product-api'
+};
--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,10 @@
-const app = require('./config/app');
+const MongoHelper = require('./../infra/helpers/mongo-helper');
+const env = require('./config/env');
 
-app.listen('3000', () => console.log('server running on port 3000'));
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app');
+
+    app.listen('3000', () => console.log('server running on port 3000'));
+  })
+  .catch(console.error);
