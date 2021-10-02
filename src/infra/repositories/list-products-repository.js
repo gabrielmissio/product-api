6ba67a8e0d5973989ac8a35429934e34815b224d
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-02 18:00:26 -0300

    feat(list-products): add ListProducts Repository

---
+++ src/infra/repositories/list-products-repository.js
@@ -0,0 +1,14 @@
+const MongoHelper = require('./../helpers/mongo-helper');
+
+class ListProductsRepository {
+  async load() {
+    const productModel = await MongoHelper.getCollection('products');
+    const projection = {
+      _id: 0
+    };
+
+    return productModel.find().project(projection).toArray();
+  };
+};
+
+module.exports = ListProductsRepository;
