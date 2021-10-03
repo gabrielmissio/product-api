d9389cbc7effd932a354b5af0dee3a9f99824dc8
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-03 14:55:28 -0300

    feat(detail-product): add DetailProduct Router

---
+++ src/presentation/routers/detail-products-router.js
@@ -0,0 +1,26 @@
+const HttpResponse = require('../helpers/http-response');
+const InvalidRequestError = require('./../errors/invalid-request-error');
+
+class DetailProductsRouter {
+  constructor({ detailProductsUseCase, requestValidator } = {}) {
+    this.detailProductsUseCase = detailProductsUseCase;
+    this.requestValidator = requestValidator;
+  }
+
+  async route(httpRequest) {
+    try {
+      const errors = this.requestValidator.validate(httpRequest.params);
+      if (errors) {
+        return HttpResponse.badRequest(new InvalidRequestError(errors));
+      }
+
+      const productDetails = await this.detailProductsUseCase.detail(httpRequest.params);
+
+      return HttpResponse.ok(productDetails);
+    } catch (error) {
+      return HttpResponse.handleError(error);
+    };
+  };
+};
+
+module.exports = DetailProductsRouter;
