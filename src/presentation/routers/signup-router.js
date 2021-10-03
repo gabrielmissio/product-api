268a2120f28342a5e35ca87355a4c4d05bbfcd76
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-02 22:55:06 -0300

    feat(signup): add Signup Router

---
+++ src/presentation/routers/signup-router.js
@@ -0,0 +1,26 @@
+const HttpResponse = require('../helpers/http-response');
+const InvalidRequestError = require('./../errors/invalid-request-error');
+
+class SignRouter {
+  constructor({ createUserUseCase, requestValidator } = {}) {
+    this.createUserUseCase = createUserUseCase;
+    this.requestValidator = requestValidator;
+  }
+
+  async route(httpRequest) {
+    try {
+      const errors = this.requestValidator.validate(httpRequest.body);
+      if (errors) {
+        return HttpResponse.badRequest(new InvalidRequestError(errors));
+      }
+
+      const user = await this.createUserUseCase.create(httpRequest.body);
+
+      return HttpResponse.created(user);
+    } catch (error) {
+      return HttpResponse.internalError();
+    };
+  };
+};
+
+module.exports = SignRouter;
