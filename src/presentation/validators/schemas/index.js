a63a551e12b5734fc94c26ba4927ea7010e59b3d
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-03 10:59:30 -0300

    refactor(validators): organize validation schemas by entity

--- src/presentation/validators/schemas/index.js
+++ src/presentation/validators/schemas/index.js
@@ -1,7 +1,7 @@
-const createProductValidatorSchema = require('./create-product-validator-schema');
-const listProductsValidatorSchema = require('./list-products-validator-schema');
-const loginValidatorSchema = require('./login-validator-schema');
-const signupValidatorSchema = require('./signup-validator-schema');
+const createProductValidatorSchema = require('./products/create-product-validator-schema');
+const listProductsValidatorSchema = require('./products/list-products-validator-schema');
+const loginValidatorSchema = require('./users/login-validator-schema');
+const signupValidatorSchema = require('./users/signup-validator-schema');
 
 module.exports = {
   createProductValidatorSchema,
