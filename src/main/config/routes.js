ee9925d0cc76697145f3172d07d0b8207d231b97
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-04 21:49:07 -0300

    feat(routes): add ProductPurchase routes

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const loginRoutes = require('./../routes/login-routes');
 const productRoutes = require('./../routes/product-routes');
+const purchasesRoutes = require('./../routes/purchases-routes');
 
 module.exports = app => {
   app.use('/', router);
   loginRoutes(router);
   productRoutes(router);
+  purchasesRoutes(router);
 };
