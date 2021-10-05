18de0e721064e187bfe576272ed245e1fd5d8495
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-04 21:58:51 -0300

    feat(enums): add ProductPurchaseStatus to enums index file

--- src/utils/enums/index.js
+++ src/utils/enums/index.js
@@ -1,7 +1,9 @@
 const errorMessages = require('./error-messages');
 const httpCodes = require('./http-codes');
+const productPurchaseStatus = require('./product-purchase-status');
 
 module.exports = {
   errorMessages,
-  httpCodes
+  httpCodes,
+  productPurchaseStatus
 };
