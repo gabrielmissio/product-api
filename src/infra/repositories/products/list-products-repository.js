5b243ae6c446f058c80bed7db5de857e60795e4c
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-07 20:58:02 -0300

    chore(list-products-repository): includ product id in the return of load method

--- src/infra/repositories/products/list-products-repository.js
+++ src/infra/repositories/products/list-products-repository.js
@@ -3,11 +3,8 @@ const MongoHelper = require('../../helpers/mongo-helper');
 class ListProductsRepository {
   async load() {
     const productModel = await MongoHelper.getCollection('products');
-    const projection = {
-      _id: 0
-    };
 
-    return productModel.find().project(projection).toArray();
+    return productModel.find().toArray();
   };
 };
 
