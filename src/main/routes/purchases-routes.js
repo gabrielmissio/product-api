2581588b765c8a9f0089501c38d41aed182454fc
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-06 21:58:53 -0300

    feeat(purchase-routes): add UpdateProductPurchase route

--- src/main/routes/purchases-routes.js
+++ src/main/routes/purchases-routes.js
@@ -1,6 +1,8 @@
 const { adapt } = require('./../adapters/express-router-adapter');
 const ProductPurchaseRouterComposer = require('./../composers/purchases/product-purchase-router-composer');
+const UpdateProductPurchaseRouterComposer = require('./../composers/purchases/update-product-purchase-router-composer');
 
 module.exports = router => {
   router.post('/purchase', adapt(ProductPurchaseRouterComposer.compose()));
+  router.put('/purchase', adapt(UpdateProductPurchaseRouterComposer.compose()));
 };
