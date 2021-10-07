73a6bbe898506a709937e24c9a081a3bfe7eb4cc
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-10-06 21:54:09 -0300

    feat(enums): add PRODUCT_PURCHASE_NOT_FOUND message

--- src/utils/enums/error-messages.js
+++ src/utils/enums/error-messages.js
@@ -1,6 +1,7 @@
 const errorMessages = {
   USER_ALREADY_EXISTS: 'user already exists',
   PRODUCT_NOT_FOUND: 'product not found',
+  PRODUCT_PURCHASE_NOT_FOUND: 'product purchase not found',
   INTERNAL_SERVER_ERROR: 'internal server error'
 };
 
